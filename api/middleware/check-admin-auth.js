d83bab1f1a7f599d0baab2db2c3094664314990c
LaluPrasad M <laluprasad.73515274@gmail.com>
2020-03-27 19:26:27 +0530

    Update check-admin-auth.js

--- api/middleware/check-admin-auth.js
+++ api/middleware/check-admin-auth.js
@@ -3,7 +3,7 @@ const Admin = require('../models/admin');
 module.exports = (req, res, next) => {
     try{
         const token = req.query.Token;
-        const decoded = jwt.verify(token, process.env.JWT_KEY);
+        const decoded = jwt.verify(token, process.env.JWT_ADMIN_KEY);
         req.userData = decoded;
         Admin.findById(req.userData.userId)
         .exec()
